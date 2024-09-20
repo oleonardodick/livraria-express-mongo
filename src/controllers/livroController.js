8f7b0f630418066c32460f3b6d7505efd0cb7721
Leonardo Bernardes <leonardod.bernardes@gmail.com>
2024-09-20 17:37:37 -0300

    Aplicado busca por filtros
    Aplicado na controller do livro, a busca variando por filtro, sendo esse filtro o titulo, editora, minimo e máximo de páginas e nome do autor.

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,5 +1,5 @@
 import NaoEncontrado from '../erros/NaoEncontrado.js';
-import { livros } from '../models/index.js';
+import { autores, livros } from '../models/index.js';
 
 class LivroController {
   static listarLivros = async (req, res, next) => {
@@ -77,17 +77,60 @@ class LivroController {
     }
   };
 
-  static listarLivroPorEditora = async (req, res, next) => {
+  static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const editora = req.query.editora;
+      const busca = await processaBusca(req.query);
+      if (busca) {
+        const livrosResultado = await livros.find(busca).populate('autor');
 
-      const livrosResultado = await livros.find({ editora: editora });
-
-      res.status(200).send(livrosResultado);
+        res.status(200).send(livrosResultado);
+      } else {
+        res.status(200).send([]);
+      }
     } catch (erro) {
       next(erro);
     }
   };
+}
+
+async function processaBusca(parametros) {
+  const { editora, titulo, minPaginas, maxPaginas, nomeAutor } = parametros;
+  let busca = {};
+  if (editora) busca.editora = editora;
+  //solução nativa do javascript
+  // const regex = new RegExp(titulo, 'i');
+
+  // if (titulo) busca.titulo = regex;
+  //solução do mongoose
+  if (titulo) busca.titulo = { $regex: titulo, $options: 'i' };
+
+  /*
+    $gte: Solução mongoose para grater then or equal
+    $lte: Solução mongoose para less then or equal
+  */
+  if (minPaginas) {
+    if (maxPaginas) {
+      busca.numeroPaginas = {
+        $gte: minPaginas,
+        $lte: maxPaginas,
+      };
+    } else {
+      busca.numeroPaginas = { $gte: minPaginas };
+    }
+  } else if (maxPaginas) {
+    busca.numeroPaginas = { $lte: maxPaginas };
+  }
+
+  if (nomeAutor) {
+    const autor = await autores.findOne({ nome: nomeAutor });
+    if (autor) {
+      busca.autor = autor._id;
+    } else {
+      busca = null;
+    }
+  }
+
+  return busca;
 }
 
 function LivroNaoLocalizado() {
